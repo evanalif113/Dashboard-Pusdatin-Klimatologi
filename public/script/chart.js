fd233c957e9b165d1ea471e0f381c4ee8fed8fbf
Evan Alif Widhyatma <evanalifwidhyatma@gmail.com>
2024-10-24 11:28:20 +0700

    Refactor charts.html: Remove unused auth-code.js script and update language attribute to Indonesian

--- public/script/chart.js
+++ public/script/chart.js
@@ -1,15 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js"; 
-import { getDatabase, ref, get, query, orderByKey, limitToLast } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-database.js";
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(); // Initialize Firebase Authentication
-const database = getDatabase(app); // Initialize Firebase Realtime Database
+const databaseUrl = "https://staklimjerukagung-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
+// ID sensor tetap sebagai id-03
+const sensorId = "id-03";
 
 // Maximum length for the data array (e.g., 60 data points)
 var maxDataLength = 60;
@@ -21,47 +20,38 @@ var humidity = [];
 var pressure = [];
 var dew = [];
 
-// Authenticate and fetch data after successful login
-signInWithEmailAndPassword(auth, userEmail, userPassword)
-    .then((userCredential) => {
-        // Signed in successfully
-        console.log("Authenticated successfully");
-        fetchLastData();
-        setInterval(fetchLastData, 15000); // Fetch new data every 15 seconds
-    })
-    .catch((error) => {
-        console.error("Authentication failed: ", error);
-    });
-
-// Function to fetch the last data using Firebase Realtime Database SDK
+// Function to fetch the last data using jQuery Ajax
 function fetchLastData() {
-    const hour = 1;
-    const fetchCount = hour * 60;
+    var hour = 1;
+    var fetchCount = hour * 60;
+    var dataRef = `${databaseUrl}auto_weather_stat/${sensorId}/data.json?orderBy="$key"&limitToLast=${fetchCount}`;
 
-    const dataRef = query(ref(database, `auto_weather_stat/id-03/data`), orderByKey(), limitToLast(fetchCount));
+    $.ajax({
+        url: dataRef,
+        type: "GET",
+        success: function(data) {
+            if (data) {
+                Object.values(data).forEach(entry => {
+                    // Convert timestamp to formatted time
+                    var timeFormatted = new Date(entry.timestamp * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
 
-    get(dataRef).then((snapshot) => {
-        if (snapshot.exists()) {
-            const data = snapshot.val();
-            Object.values(data).forEach(entry => {
-                // Convert timestamp to formatted time
-                const timeFormatted = new Date(entry.timestamp * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
+                    // Update arrays with new data (shift array when data exceeds max length)
+                    updateDataArray(timestamps, timeFormatted);
+                    updateDataArray(temperatures, entry.temperature);
+                    updateDataArray(humidity, entry.humidity);
+                    updateDataArray(pressure, entry.pressure);
+                    updateDataArray(dew, entry.dew);
+                });
 
-                // Update arrays with new data (shift array when data exceeds max length)
-                updateDataArray(timestamps, timeFormatted);
-                updateDataArray(temperatures, entry.temperature);
-                updateDataArray(humidity, entry.humidity);
-                updateDataArray(pressure, entry.pressure);
-                updateDataArray(dew, entry.dew);
-            });
-
-            // Update charts with the new data
-            updateCharts();
-        } else {
-            console.warn("Tidak ada data yang tersedia.");
+                // Update charts with the new data
+                updateCharts();
+            } else {
+                console.warn("Tidak ada data yang tersedia.");
+            }
+        },
+        error: function(error) {
+            console.error("Error fetching data: ", error);
         }
-    }).catch((error) => {
-        console.error("Error fetching data: ", error);
     });
 }
 
@@ -75,7 +65,7 @@ function updateDataArray(array, newValue) {
 
 // Function to plot temperature chart
 function plotTemperatureChart() {
-    const trace = {
+    var trace = {
         x: timestamps,
         y: temperatures,
         type: 'scatter',
@@ -84,7 +74,7 @@ function plotTemperatureChart() {
         line: { color: '#C70039' }
     };
 
-    const layout = {
+    var layout = {
         title: 'Suhu Lingkungan (°C)',
         xaxis: { title: 'Waktu' },
         yaxis: { title: 'Suhu Lingkungan (°C)' },
@@ -96,7 +86,7 @@ function plotTemperatureChart() {
 
 // Function to plot humidity chart
 function plotHumidityChart() {
-    const trace = {
+    var trace = {
         x: timestamps,
         y: humidity,
         type: 'scatter',
@@ -105,7 +95,7 @@ function plotHumidityChart() {
         line: { color: '006BFF' }
     };
 
-    const layout = {
+    var layout = {
         title: 'Kelembapan Relatif (%)',
         xaxis: { title: 'Waktu' },
         yaxis: { title: 'Kelembapan Relatif (%)' },
@@ -117,7 +107,7 @@ function plotHumidityChart() {
 
 // Function to plot dew point chart
 function plotDewChart() {
-    const trace = {
+    var trace = {
         x: timestamps,
         y: dew,
         type: 'scatter',
@@ -126,7 +116,7 @@ function plotDewChart() {
         line: { color: '4F75FF' }
     };
 
-    const layout = {
+    var layout = {
         title: 'Titik Embun (°C)',
         xaxis: { title: 'Waktu' },
         yaxis: { title: 'Titik Embun (°C)' },
@@ -138,7 +128,7 @@ function plotDewChart() {
 
 // Function to plot pressure chart
 function plotPressureChart() {
-    const trace = {
+    var trace = {
         x: timestamps,
         y: pressure,
         type: 'scatter',
@@ -147,7 +137,7 @@ function plotPressureChart() {
         line: { color: '15B392' }
     };
 
-    const layout = {
+    var layout = {
         title: 'Tekanan Udara (hPa)',
         xaxis: { title: 'Waktu' },
         yaxis: { title: 'Tekanan Udara (hPa)' },
@@ -159,25 +149,25 @@ function plotPressureChart() {
 
 // Function to update charts dynamically
 function updateCharts() {
-    const data_update_temp = { 
+    var data_update_temp = { 
         x: [timestamps], 
         y: [temperatures] 
     };
     Plotly.update('temperature-chart', data_update_temp);
 
-    const data_update_humid = { 
+    var data_update_humid = { 
         x: [timestamps], 
         y: [humidity] 
     };
     Plotly.update('humidity-chart', data_update_humid);
 
-    const data_update_press = { 
+    var data_update_press = { 
         x: [timestamps], 
         y: [pressure] 
     };
     Plotly.update('pressure-chart', data_update_press);
 
-    const data_update_dew = { 
+    var data_update_dew = { 
         x: [timestamps], 
         y: [dew] 
     };
@@ -190,4 +180,7 @@ $(document).ready(function() {
     plotHumidityChart();
     plotPressureChart();
     plotDewChart();
+
+    fetchLastData();
+    setInterval(fetchLastData, 30000); // Fetch new data every...
 });
