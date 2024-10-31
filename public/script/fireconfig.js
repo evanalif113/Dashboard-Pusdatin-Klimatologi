31ebffc1b6f486a7f2d2a1ce77e726b2517affe3
evanalif113 <evanalifwidhyatma@gmail.com>
2024-10-31 10:23:04 +0700

    Refactor gitignore: Remove public/script/fireconfig.js

---
+++ public/script/fireconfig.js
@@ -0,0 +1,24 @@
+// fireconfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
+
+// Konfigurasi Firebase
+// firebaseConfig.js
+export const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+  
+
+// Inisialisasi Firebase App
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export {analytics};
+export default app;  // Menggunakan default export untuk app
