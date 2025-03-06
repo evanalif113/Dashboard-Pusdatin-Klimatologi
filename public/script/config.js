0f9d01e29394921d876e44e810a33e131d9eee79
Widhyatma Sensei <evanalifwidhyatma@gmail.com>
2025-03-06 17:51:25 +0700

    Add Firebase configuration and initialization script

---
+++ public/script/config.js
@@ -0,0 +1,22 @@
+// fireconfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+
+// Konfigurasi Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDalcCwwOthPMjC3umkpQECqlQQj699FTY",
+    authDomain: "staklimjerukagung.firebaseapp.com",
+    databaseURL: "https://staklimjerukagung-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "staklimjerukagung",
+    storageBucket: "staklimjerukagung.appspot.com",
+    messagingSenderId: "763003005982",
+    appId: "1:763003005982:web:8ce295eda92c6b9112d20f",
+    measurementId: "G-DRL05TMRNT"
+};
+
+// Inisialisasi Firebase App
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export {analytics};
+export default app;  // Menggunakan default export untuk app
